0337cabc7738ab68ec96ecc66999b99d0e9e24d0
Sahilbalhra <sahilbalhra0@gmail.com>
2022-04-04 17:18:43 +0530

    navbar added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -22,22 +21,7 @@ export default function Home() {
           href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTQ-OdYjeNTHrpuh7lMt5TR2DdySHXaz2sSA&usqp=CAU"
         />
       </Head>
-      <nav className={styles.mainNav}>
-        <ul>
-          <Link href="/home">
-            <li>Home</li>
-          </Link>
-          <Link href="/about">
-            <li>About</li>
-          </Link>
-          <Link href="/blog">
-            <li>Blog</li>
-          </Link>
-          <Link href="/contact">
-            <li>Contact</li>
-          </Link>
-        </ul>
-      </nav>
+     
       <main className={styles.main}>
         <h1 className={styles.title}>Coding Blogs</h1>
         <div className={styles.imgWrapper}>
@@ -50,7 +34,7 @@ export default function Home() {
         </div>
         <p className={styles.description}>A blog for coders</p>
         <div className="blogs">
-          <h2>Popular Blogs</h2>
+          <h2>Latest Blogs</h2>
           <div className="blogItem">
             <h3>How to learn javaScript in 2022</h3>
             <p>JavaScript is a object oriented scripting language</p>
