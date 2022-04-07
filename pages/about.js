01e737415858cf1f01ac69368806ef0d41c89c2b
Sahilbalhra <sahilbalhra0@gmail.com>
2022-04-07 23:02:10 +0530

    about page added

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import styles from "../styles/About.module.css";
 
 const About = () => {
   return (
-    <div>about</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.center}>About Us CodingBlog</h1>
+      <h2>Introduction</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti sunt
+        fuga quae cum blanditiis culpa! Nulla magni eum qui, quo, at atque
+        tempore fugit quia itaque illo perferendis nihil iste! Rem perspiciatis
+        iure in quisquam quos molestias, tenetur reprehenderit assumenda labore
+        non? Sit repudiandae soluta similique hic, quisquam velit eveniet,
+        aliquid possimus quam distinctio deleniti. Reiciendis distinctio
+        dolorem, similique quibusdam voluptatibus est libero numquam quam dolore
+        nobis ipsam nesciunt vel iste. Deleniti, id. Suscipit, quasi. Doloribus
+        id esse maxime? Placeat in enim sapiente iste culpa tenetur, nulla,
+        explicabo cupiditate cum eaque ut, harum quisquam amet commodi velit
+        consequatur maiores error. Suscipit vel quidem impedit, nihil aliquam
+        modi debitis aperiam numquam quasi perferendis cum at? Suscipit qui
+        similique tenetur eaque dolore illum sapiente aliquam obcaecati sint,
+        porro debitis quia magni dolorum, perspiciatis asperiores dolores
+        voluptatum nesciunt saepe maxime sit architecto ut rerum recusandae
+        omnis? Minus, facere placeat? Dolor facilis tempore repudiandae cum
+        suscipit eaque ab placeat. Porro voluptatum maxime error hic iusto natus
+        cupiditate eius, dolore ut deleniti officia dolor facere eum assumenda
+        ratione odit dignissimos mollitia iure ipsum nobis, amet architecto
+        velit necessitatibus. Tempore laborum praesentium inventore, temporibus
+        reprehenderit facere nihil eveniet ipsum molestiae fuga eaque,
+        accusantium.
+      </p>
+      <h2>Services Offered</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus alias
+        incidunt eum mollitia numquam asperiores reiciendis, neque impedit
+        placeat? Eaque omnis iste accusamus distinctio sequi tempore consectetur
+        optio architecto doloremque a soluta cumque quae assumenda molestias
+        doloribus blanditiis nisi, veritatis vel temporibus dolorem error
+        incidunt recusandae quasi reiciendis! Minima, deleniti dolorem pariatur
+        veniam excepturi sed culpa quas provident eos cum ut quod nam ad et
+        maiores quia iste doloremque odit. Sapiente numquam dolor id iusto at
+        assumenda repudiandae necessitatibus provident, architecto atque, vel
+        maiores! Nam non odit, nemo blanditiis laudantium culpa nostrum natus.
+        Rem labore quo nesciunt inventore veritatis dolore!
+      </p>
+      <h2>Contact us</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque nisi,
+        provident natus officiis excepturi consequatur cumque quisquam
+        aspernatur veniam neque quas sunt consequuntur harum. Sapiente
+        voluptatum optio sequi quaerat sit a, placeat voluptas magni numquam
+        iure necessitatibus facilis quae cumque deleniti ipsa aperiam, aliquam
+        architecto fugit. Voluptas laborum dolor praesentium! Minus vitae
+        laudantium velit, laboriosam quos ut animi omnis fugiat eum ratione
+        exercitationem libero voluptatem reprehenderit unde molestias fuga
+        perspiciatis a nisi dolorem architecto eos. Dolores molestias quisquam
+        ut reiciendis.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
